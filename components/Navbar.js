3ba2e44447745ebdefe20cf3c755148089aefaa6
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-10-07 00:38:22 +0530

    c

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import Link from 'next/link'
 const Navbar = () => {
   return (
-    <div>
+    <div className='text-[#252525] font-bold'>
+        <div className='fixed bottom-[3vw] right-[3vw] h-[4.5vw] cursor-pointer text-center w-[4.5vw] shadow-lg rounded-br-[3.5vw] hover:bg-[#252525] hover:rounded-tl-[3.5vw] text-[2.5vw] hover:rounded-br-none hover:text-[#fbfbfb] transition-all duration-700 bg-[#D5F3C1]'>↑</div>
         <div style={{fontFamily: `'Montserrat',sans-serif`}} className='flex text-[1.25vw] bg-white justify-between mx-[8vw]'>
                 <img className='h-[6.25vw] ml-[4vw]' src={"/logo.png"} alt="logo"></img>
-            <div className='flex justify-evenly items-center w-2/4 '>
+            <div className='flex justify-evenly items-center w-3/4 '>
                 <Link href={"/"}>Home</Link>
                 <Link href={"/about"}>About Us</Link>
                 <Link href={"/"}>Products</Link>
